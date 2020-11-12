20801c7d7c9fd1d58eb0ea533f101c98b6628466
Diego Henriquez <df.henriquez@hotmail.com>
2020-11-12 14:40:33 -0600

    Agregando migración inicial, también se ha agregado nuevo controlador para el manejo de autores.

---
+++ CatalogoWebApp/Controllers/AutoresController.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections.Generic;
+using CatalogoWebApp.DataAccess;
+using CatalogoWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoWebApp.Controllers
+{
+    public class AutoresController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IList<Carrera> _carreras;
+
+        public AutoresController(AppDbContext context)
+        {
+            _context = context;
+            _carreras = _context.Carreras.OrderBy(c => c.Nombre).ToList();
+
+        }
+
+        // GET: Autores
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Autores.Include(a => a.Carrera);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Autores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autores
+                .Include(a => a.Carrera)
+                .ThenInclude(a=>a.Facultad)
+                .FirstOrDefaultAsync(m => m.AutorId == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return View(autor);
+        }
+
+        // GET: Autores/Create
+        public IActionResult Create()
+        {
+            ViewData["CarreraId"] = new SelectList(_carreras, "CarreraId", "Nombre");
+            return View();
+        }
+
+        // POST: Autores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AutorId,Codigo,Nombres,Apellidos,CarreraId")] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(autor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CarreraId"] = new SelectList(_carreras, "CarreraId", "Nombre", autor.CarreraId);
+            return View(autor);
+        }
+
+        // GET: Autores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autores.FindAsync(id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            ViewData["CarreraId"] = new SelectList(_carreras, "CarreraId", "Nombre", autor.CarreraId);
+            return View(autor);
+        }
+
+        // POST: Autores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AutorId,Codigo,Nombres,Apellidos,CarreraId")] Autor autor)
+        {
+            if (id != autor.AutorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(autor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AutorExists(autor.AutorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CarreraId"] = new SelectList(_carreras, "CarreraId", "Nombre", autor.CarreraId);
+            return View(autor);
+        }
+
+        // GET: Autores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var autor = await _context.Autores
+                .Include(a => a.Carrera)
+                .FirstOrDefaultAsync(m => m.AutorId == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return View(autor);
+        }
+
+        // POST: Autores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var autor = await _context.Autores.FindAsync(id);
+            _context.Autores.Remove(autor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AutorExists(int id)
+        {
+            return _context.Autores.Any(e => e.AutorId == id);
+        }
+    }
+}
---
+++ CatalogoWebApp/Migrations/20201112195952_Init.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatalogoWebApp.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Facultades",
+                columns: table => new
+                {
+                    FacultadId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Facultades", x => x.FacultadId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tipos",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tipos", x => x.TipoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carreras",
+                columns: table => new
+                {
+                    CarreraId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FacultadId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carreras", x => x.CarreraId);
+                    table.ForeignKey(
+                        name: "FK_Carreras_Facultades_FacultadId",
+                        column: x => x.FacultadId,
+                        principalTable: "Facultades",
+                        principalColumn: "FacultadId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    AutorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarreraId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.AutorId);
+                    table.ForeignKey(
+                        name: "FK_Autores_Carreras_CarreraId",
+                        column: x => x.CarreraId,
+                        principalTable: "Carreras",
+                        principalColumn: "CarreraId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrabajosDeGraduacion",
+                columns: table => new
+                {
+                    TrabajoDeGraduacionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AutorId = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PathImagen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    CarreraId = table.Column<int>(type: "int", nullable: false),
+                    TipoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrabajosDeGraduacion", x => x.TrabajoDeGraduacionId);
+                    table.ForeignKey(
+                        name: "FK_TrabajosDeGraduacion_Autores_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "Autores",
+                        principalColumn: "AutorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TrabajosDeGraduacion_Tipos_TipoId",
+                        column: x => x.TipoId,
+                        principalTable: "Tipos",
+                        principalColumn: "TipoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Facultades",
+                columns: new[] { "FacultadId", "Codigo", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, "NA", "NO ASIGNADA" },
+                    { 100, "100", "CIENCIAS  Y HUMANIDADES" },
+                    { 500, "500", "FACULTAD DE CIENCIAS DE LA SALUD" },
+                    { 600, "600", "INGENIERIA Y ARQUITECTURA" },
+                    { 800, "800", "CIENCIAS EMPRESARIALES" },
+                    { 900, "900", "DEPARTAMENTO DE IDIOMAS" },
+                    { 906, null, "BIBLIOTECA" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tipos",
+                columns: new[] { "TipoId", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, "Pasantia" },
+                    { 2, "Proyecto" },
+                    { 3, "Tesis" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Carreras",
+                columns: new[] { "CarreraId", "Codigo", "FacultadId", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, "000", 1, "GENERAL" },
+                    { 25, "209", 600, "INGENIERIA EN TECNOLOGIA Y PROCESAMIENTO DE ALIMENTOS" },
+                    { 30, "601", 600, "INGENIERIA INDUSTRIAL" },
+                    { 31, "602", 600, "ARQUITECTURA" },
+                    { 32, "603", 600, "INGENIERIA CIVIL" },
+                    { 33, "604", 600, "INGENIERIA CIVIL SANEAMIENTO AMBIENTAL" },
+                    { 34, "605", 600, "INGENIERIA EN SISTEMAS INFORMATICOS" },
+                    { 35, "606", 600, "INGENIERIA AGRONOMICA" },
+                    { 36, "607", 600, "INGENIERIA EN TELECOMUNICACIONES Y REDES" },
+                    { 9, "110", 500, "DOCTORADO EN MEDICINA" },
+                    { 37, "608", 600, "INGENIERIA EN DESARROLLO DE SOFTWARE" },
+                    { 29, "410", 800, "LICENCIATURA EN SISTEMAS INFORMATICOS ADMINISTRATIVOS" },
+                    { 40, "801", 800, "LICENCIATURA EN ADMINISTRACION DE EMPRESAS" },
+                    { 41, "802", 800, "LICENCIATURA EN MERCADEO Y NEGOCIOS INTERNACIONALES" },
+                    { 42, "803", 800, "LICENCIATURA EN GESTION Y DESARROLLO TURISTICO" },
+                    { 43, "812", 800, "LICENCIATURA EN CONTADURIA PUBLICA" },
+                    { 44, "890", 800, "POSTGRADO EN ESTRATEGIAS PARA LA COMPETITIVIDAD" },
+                    { 46, "901", 800, "MAESTRIA EN DIRECCION ESTRATEGICA DE EMPRESAS" },
+                    { 51, "906", 800, "MAESTRIA EN GESTION Y DESARROLLO TURISTICO" },
+                    { 50, "905", 600, "MAESTRIA EN GERENCIA Y GESTION AMBIENTAL" },
+                    { 7, "103", 500, "TECNICO EN ENFERMERIA" },
+                    { 6, "102", 500, "LICENCIATURA EN ENFERMERIA" },
+                    { 52, "982", 100, "CURSO DE FORMACION PEDAGOGICA PARA PROFESIONALES" },
+                    { 5, "101", 100, "LICENCIATURA EN PERIODISMO Y COMUNICACIÓN AUDIOVISUAL" },
+                    { 8, "104", 100, "LICENCIATURA EN CIENCIAS DE LA EDUCACION CON ESPECIALIDAD EN IDIOMA INGLÉS" },
+                    { 10, "119", 100, "LICENCIATURA EN CIENCIAS DE LA EDUCACION CON ESPECIALIDAD EN EDUCACION BASICA" },
+                    { 11, "120", 100, "PROFESORADO EN EDUCACION BASICA PARA PRIMERO Y SEGUNDO CICLO" },
+                    { 12, "127", 100, "PROFESORADO EN EDUCACIÓN PARVULARIA" },
+                    { 13, "128", 100, "LICENCIATURA EN CIENCIAS RELIGIOSAS" },
+                    { 14, "129", 100, "LICENCIATURA EN CIENCIAS DE LA EDUCACION CON ESPECIALIDAD EN EDUCACION PARVULARIA" },
+                    { 15, "130", 100, "LICENCIATURA EN EDUCACION INICIAL Y  PARVULARIA" },
+                    { 16, "131", 100, "LICENCIATURA EN CIENCIAS DE LA EDUCACION ESPECIALIDAD EN MATEMATICA SEMIPRESENCIAL" },
+                    { 17, "132", 100, "LICENCIATURA EN CIENCIAS DE LA EDUCACION ESPECIALIDAD EN DIRECCION Y ADMINISTRACION ESCOLAR - SEMIPRESENCIAL" },
+                    { 18, "133", 100, "LICENCIATURA EN CIENCIAS DE LA EDUCACION ESPECIALIDAD EN EDUCACION BASICA SEMIPRESENCIAL" },
+                    { 19, "135", 100, "LICENCIATURA EN IDIOMA INGLES (SEMIPRESENCIAL)" },
+                    { 20, "140", 100, "PROFESORADO EN EDUCACION  BASICA PARA PRIMERO Y SEGUNDO CICLOS" },
+                    { 21, "144", 100, "PROFESORADO Y LICENCIATURA EN EDUCACION INICIAL Y PARVULARIA" },
+                    { 22, "145", 100, "LICENCIATURA EN IDIOMA INGLES" },
+                    { 23, "160", 100, "LICENCIATURA EN DISEÑO GRAFICO PUBLICITARIO" },
+                    { 26, "301", 100, "LICENCIATURA EN CIENCIAS JURIDICAS" },
+                    { 47, "902", 100, "MAESTRIA EN ASESORIA EDUCATIVA" },
+                    { 49, "904", 100, "MAESTRIA EN ATENCION INTEGRAL DE LA PRIMERA INFANCIA" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Carreras",
+                columns: new[] { "CarreraId", "Codigo", "FacultadId", "Nombre" },
+                values: new object[] { 56, "805", 800, "LICENCIATURA EN ADMINISTRACION DE EMPRESAS - SEMIPRESENCIAL" });
+
+            migrationBuilder.InsertData(
+                table: "Carreras",
+                columns: new[] { "CarreraId", "Codigo", "FacultadId", "Nombre" },
+                values: new object[] { 45, "900", 900, "DEPARTAMENTO DE IDIOMAS" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Autores_CarreraId",
+                table: "Autores",
+                column: "CarreraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carreras_FacultadId",
+                table: "Carreras",
+                column: "FacultadId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrabajosDeGraduacion_AutorId",
+                table: "TrabajosDeGraduacion",
+                column: "AutorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrabajosDeGraduacion_TipoId",
+                table: "TrabajosDeGraduacion",
+                column: "TipoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrabajosDeGraduacion");
+
+            migrationBuilder.DropTable(
+                name: "Autores");
+
+            migrationBuilder.DropTable(
+                name: "Tipos");
+
+            migrationBuilder.DropTable(
+                name: "Carreras");
+
+            migrationBuilder.DropTable(
+                name: "Facultades");
+        }
+    }
+}
