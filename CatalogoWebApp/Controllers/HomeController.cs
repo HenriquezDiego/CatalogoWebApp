d0b81aaa63298183d764fec9b3e238b417c75eea
Diego Henriquez <df.henriquez@hotmail.com>
2021-10-05 15:15:01 -0600

    Se ha iniciado el desarrollo de la busque en la pagina principal (Catalogo).

--- CatalogoWebApp/Controllers/HomeController.cs
+++ CatalogoWebApp/Controllers/HomeController.cs
@@ -14,14 +14,20 @@ namespace AdminLTE.Controllers
         {
             _appDbContext = appDbContext;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string search)
         {
             var trabajosDeGraduacion = await _appDbContext.TrabajosDeGraduacion
                 .Include(t => t.Autor)
                 .ThenInclude(t=>t.Carrera)
                 .OrderBy(t=>t.Titulo)
                 .ToListAsync();
-
+            search = search?.ToLower();
+            if (!string.IsNullOrEmpty(search))
+            {
+                trabajosDeGraduacion = trabajosDeGraduacion.Where(t =>
+                    t.Descripcion.ToLower().Contains(search) || t.Autor.Nombres.ToLower().Contains(search) ||
+                    t.Autor.Apellidos.ToLower().Contains(search)||t.Fecha.Year.ToString() == search).ToList();
+            }
             return View(trabajosDeGraduacion);
         }
 
