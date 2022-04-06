c140520faf68308b78515f1058e4ca4a971889b4
Diego Henriquez <df.henriquez@hotmail.com>
2022-04-06 16:09:44 -0600

    agregando atributo BsonElement..

--- CatalogoWebApp/Models/NoSQL/Facultad.cs
+++ CatalogoWebApp/Models/NoSQL/Facultad.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using MongoDB.Bson.Serialization.Attributes;
 
 namespace CatalogoWebApp.Models.NoSQL
 {
     public class Facultad : Documento
     {
+        [BsonElement("codigo")]
         public string Codigo { get; set; }
+        [BsonElement("nombre")]
         public string Nombre { get; set; }
 
     }
